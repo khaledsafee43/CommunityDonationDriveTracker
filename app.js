7249ce09556314d32f38e64feea0feae5b868d2d
Khaled Saifee <saifeekhaled1212@gmail.com>
2026-09-07 23:15:31 -0700

    Updated all the files in this project

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-let cards = document.getElementById("cards-container");
-fetch("data.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    const cardss = data.map((jsonData) => {
-      return `
-            <div class="card">
-                <h3 class="item-title">${jsonData.item}</h3>
-              <p>دریافت‌شده: <span>60</span> از <span>100</span></p>
-              <p>باقی‌مانده: <span>40</span></p>
-              <p>پیشرفت: <span>60%</span></p>
-              <span class="badge warning">هنوز نیاز است</span>
-            </div>
-        `;
-    });
-    cards.innerHTML = cardss.json("")
-  });
+const state = {
+  items: [],
+  sessionContributions: [],
+  nextSessionId: 1,
+  loadSucceeded: false,
+  searchTerm: "",
+  statusFilter: "all",
+  donorFilter: ""
+};
+
