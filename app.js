30bb3a89c22fa64e1c9899b44af04985163de249
Khaled Saifee <saifeekhaled1212@gmail.com>
2026-09-07 23:41:54 -0700

    selected all html tags successfully

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ const state = {
   donorFilter: ""
 };
 
+let loadState = document.getElementById("loading-state");
+let errorState = document.getElementById("error-state");
+let errorBTN = document.getElementById("retry-btn");
+let appContent = document.getElementById("app-content");
+let statMet = document.getElementById("stat-met");
+let statTotalItem = document.getElementById("stat-total-items");
+let statSessionCount = document.getElementById("stat-session-count");
+let searchInput = document.getElementById("search-input");
+let statFilter = document.getElementById("status-filter");
+let cardsContainer = document.getElementById("cards-container");
+let cardsEmptyStat = document.getElementById("cards-empty-state");
+let contributionForm = document.getElementById("contribution-form");
+let donorName = document.getElementById("donor-name");
+let addItem = document.getElementById("add-item");
+let quantityInput =document.getElementById("quantity-input");
+let submitContributionBTN = document.getElementById("submit-contribution-btn");
+let formMessage = document.getElementById("form-message");
+let donorFilter = document.getElementById("donor-filter");
+let sessionTableBody = document.getElementById("session-table-body");
+let sessionEmptyState = document.getElementById("session-empty-state");
